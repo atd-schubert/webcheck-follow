b476bee689bd999c4bbd455b8bc601b7d8f56795
Arne Schubert <atd.schubert@gmail.com>
2015-09-25 10:08:24 +0200

    #4, #3: Remove timeouts and add hash tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 /*jslint node:true*/
 
-/*global describe, it, before, after, beforeEach, afterEach*/
+/*global describe, it, before, after, beforeEanonsenseach*/
 
 'use strict';
 
@@ -25,6 +25,9 @@ describe('Mirror Plugin', function () {
         app.get('/test.html', function (req, res) {
             res.send('<html><head></head><body><a href="href-works.txt">Link</a><img src="src-works.txt" /> Just a fix URL: https://github.com/atd-schubert</body></html>');
         });
+        app.get('/hash.html', function (req, res) {
+            res.send('<html><head></head><body><a href="href-works.txt#hash">Link</a><img src="src-works.txt#hash" /> Just a fix URL: https://github.com/atd-schubert#index url("css-works.txt#hash")</body></html>');
+        });
         /*jslint unparam: false*/
 
         freeport(function (err, p) {
@@ -47,76 +50,103 @@ describe('Mirror Plugin', function () {
         });
 
         it('should follow src, href and full urls', function (done) {
-            var src, href, full;
-            webcheck.on('crawl', function (result) {
-                if (result.url === 'https://github.com/atd-schubert') {
+            var src, href, full, listener;
+
+            listener = function (settings) {
+                if (settings.url === 'https://github.com/atd-schubert') {
+                    settings.preventCrawl = true;
                     full = true;
-                } else if (result.url === 'http://localhost:' + port + '/src-works.txt') {
+                } else if (settings.url === 'http://localhost:' + port + '/src-works.txt') {
+                    settings.preventCrawl = true;
                     src = true;
-                } else if (result.url === 'http://localhost:' + port + '/href-works.txt') {
+                } else if (settings.url === 'http://localhost:' + port + '/href-works.txt') {
+                    settings.preventCrawl = true;
                     href = true;
                 }
-            });
+                if (href && src && full) {
+                    webcheck.removeListener('crawl', listener);
+                    return done();
+                }
+            };
+            webcheck.on('crawl', listener);
             webcheck.crawl({
                 url: 'http://localhost:' + port + '/test.html'
             }, function (err) {
                 if (err) {
                     return done(err);
                 }
-                setTimeout(function(){
-                    if (href && src && full) {
-                        return done();
-                    }
-                    return done(new Error('A resource was not found'));
-                }, 1);
-
-
             });
         });
         it('should find urls in css', function (done) {
-            var found;
-            webcheck.on('crawl', function (result) {
-                if (result.url === 'http://localhost:' + port + '/works.txt') {
-                    found = true;
+            var listener;
+
+            listener = function (settings) {
+                if (settings.url === 'http://localhost:' + port + '/works.txt') {
+                    settings.preventCrawl = true;
+                    webcheck.removeListener('crawl', listener);
+                    return done();
                 }
-            });
+            };
+            webcheck.on('crawl', listener);
             webcheck.crawl({
                 url: 'http://localhost:' + port + '/test.css'
             }, function (err) {
                 if (err) {
                     return done(err);
                 }
-                setTimeout(function(){
-                    if (found) {
-                        return done();
-                    }
-                    return done(new Error('A resource was not found'));
-                }, 1);
-
-
             });
         });
         it('should find urls text', function (done) {
-            var found;
-            webcheck.on('crawl', function (result) {
-                if (result.url === 'https://github.com/atd-schubert') {
-                    found = true;
+            var found, listener;
+            listener = function (settings) {
+                if (settings.url === 'https://github.com/atd-schubert') {
+                    settings.preventCrawl = true;
+                    if (!found) {
+                        found = true;
+                    }
+                    webcheck.removeListener('crawl', listener);
+                    return done();
                 }
-            });
+            };
+            webcheck.on('crawl', listener);
             webcheck.crawl({
                 url: 'http://localhost:' + port + '/test.txt'
             }, function (err) {
                 if (err) {
                     return done(err);
                 }
-                setTimeout(function(){
-                    if (found) {
-                        return done();
-                    }
-                    return done(new Error('A resource was not found'));
-                }, 1);
+            });
+        });
+        it('should find urls with hash', function (done) {
+            var full, src, href, css, listener;
 
+            listener = function (settings) {
+                if (settings.url === 'https://github.com/atd-schubert') {
+                    settings.preventCrawl = true;
+                    full = true;
+                } else if (settings.url === 'http://localhost:' + port + '/src-works.txt') {
+                    settings.preventCrawl = true;
+                    src = true;
+                } else if (settings.url === 'http://localhost:' + port + '/href-works.txt') {
+                    settings.preventCrawl = true;
+                    href = true;
+                } else if (settings.url === 'http://localhost:' + port + '/css-works.txt') {
+                    settings.preventCrawl = true;
+                    css = true;
+                }
 
+                if (href && src && full && css) {
+                    webcheck.removeListener('crawl', listener);
+                    return done();
+                }
+            };
+            webcheck.on('crawl', listener);
+            webcheck.crawl({
+                url: 'http://localhost:' + port + '/hash.html'
+            }, function (err) {
+                if (err) {
+                    return done(err);
+                }
             });
         });
     });
